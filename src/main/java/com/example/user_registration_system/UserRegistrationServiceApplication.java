5943d3951ff15da7b87dc8aee98aefe3766a17e9
Cheng Wang <43586404+chengw123@users.noreply.github.com>
2025-03-04 13:23:26 -0500

    initial

---
+++ src/main/java/com/example/user_registration_system/UserRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_registration_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/user_registration_system/UserRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_registration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
